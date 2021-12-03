9773175a596b1f65b5b537b2659a35357acf4c35
Justin Lin <61717681+justinlin099@users.noreply.github.com>
2021-12-04 00:26:30 +0800

    Add Script for Preview Version
    Add Script for Preview Version

--- Windows Terminal Preview Installer.ps1
+++ Windows Terminal Preview Installer.ps1
@@ -1,6 +1,6 @@
-#Download and deploy latest Windows Terminal release from github
+#Download and deploy latest Windows Terminal Prerelease from github
 
-Write-Host Windows Terminal Installer by Justin Lin V1.00
+Write-Host Windows Terminal Preview Installer by Justin Lin V1.00
 
 $repo = "microsoft/terminal"
 $releases = "https://api.github.com/repos/$repo/releases"
@@ -9,17 +9,17 @@ $prereleaselst = (Invoke-WebRequest $releases | ConvertFrom-Json).prerelease
 
 for (($i = 0); $i -lt $prereleaselst.count ; $i++ )
 {
-	if ($prereleaselst[$i] -match "False"){
-		$stablereleaseindex = $i
+	if ($prereleaselst[$i] -match "True"){
+		$prereleaseindex = $i
 		break
 	}
 }
 
-$tag = $taglst[$stablereleaseindex]
-$file =  "Microsoft.WindowsTerminal_"+$tag.Remove(0,1)+"_8wekyb3d8bbwe.msixbundle"
+$tag = $taglst[$prereleaseindex]
+$file =  "Microsoft.WindowsTerminalPreview_"+$tag.Remove(0,1)+"_8wekyb3d8bbwe.msixbundle"
 $downloadpath = "https://github.com/$repo/releases/download/$tag/$file"
 
-Write-Host Downloading Latest Version of Windows Terminal
+Write-Host Downloading Latest Version of Windows Terminal Preview
 Invoke-WebRequest $downloadpath -Out $file
 
 Write-Host Installing Windows Terminal $tag
